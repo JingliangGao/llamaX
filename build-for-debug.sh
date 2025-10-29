8a9d0887c13f2e134b0fdf3ce5e9615f42885b15
Your Name <you@example.com>
2025-10-29 11:09:03 +0800

    [feat]: intial dev-porn branch

---
+++ build-for-debug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# set variables
+PROJECT_DIR=$(pwd)
+
+# create build directory
+if [ ! -d "build_porn" ]; then
+    mkdir build_porn
+fi
+cd build_porn
+
+# build project
+cmake .. \
+        -DCMAKE_C_COMPILER=cc \
+        -DCMAKE_CXX_COMPILER=g++ \
+        -DCMAKE_CXX_FLAGS="" \
+        -DCMAKE_C_FLAGS="" \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DCMAKE_INSTALL_LIBDIR=/usr/lib \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DGGML_CUDA=OFF \
+        -DGGML_NATIVE=OFF \
+        -DGGML_BACKEND_DL=ON \
+        -DGGML_CPU_ALL_VARIANTS=ON \
+        -DGGML_AVX512=OFF \
+        -DGGML_PONN=ON
+make -j$(nproc)
+
---
+++ docs/backend/PORN.md
@@ -0,0 +1,15 @@
+# llama.cpp for PORN
+
+ - [Background](#background)
+ - [News](#news)
+ - [OS](#os)
+ - [Hardware](#hardware)
+ - [Model Supports](#model-supports)
+ - [DataType Supports](#datatype-supports)
+ - [Docker](#docker)
+ - [Linux](#linux)
+ - [Environment variable setup](#environment-variable-setup)
+ - [TODO](#todo)
+
+
+## Background
